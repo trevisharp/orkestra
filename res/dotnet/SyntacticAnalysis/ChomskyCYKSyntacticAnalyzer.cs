1b3e7374a7cfb511e7195f1ee3cc5a7ee558c347
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2023-07-07 09:35:29 -0300

    Start of ChomskyCYK implementation

--- res/dotnet/SyntacticAnalysis/ChomskyCYKSyntacticAnalyzer.cs
+++ res/dotnet/SyntacticAnalysis/ChomskyCYKSyntacticAnalyzer.cs
@@ -1,6 +1,7 @@
 /* Author:  Leonardo Trevisan Silio
  * Date:    03/07/2023
  */
+using System.Linq;
 using System.Collections.Generic;
 
 namespace Orkestra.SyntacticAnalysis;
@@ -15,15 +16,39 @@ public class ChomskyCYKSyntacticAnalyzer : ISyntacticAnalyzer
     public List<Rule> Rules { get; private set; } = new List<Rule>();
     public void Add(Rule rule) => this.Rules.Add(rule);
 
-    public ExpressionTree Parse(IEnumerable<Token> tokens)
+    public ExpressionTree Parse(IEnumerable<Token> tokenCollection)
     {
-        var x = reduct(tokens);
+        var tokens = tokenCollection.ToArray();
+        var rules = Rules.ToArray();
+
+        int n = tokens.Length;
+        int r = rules.Length;
+
+        bool[] P = new bool[n * n * r];
+        List<int>[] B = new List<int>[n * n * r];
+
+        for (int i = 0; i < n; i++)
+        {
+            var token = tokens[i];
+            for (int j = 0; j < r; j++)
+            {
+                var rule = rules[j];
+
+                foreach (var sbr in rule.SubRules)
+                {
+                    var ruleTokens = sbr.RuleTokens;
+                    if (ruleTokens.Count() != 1)
+                        continue;
+                    
+                    var ruleToken = ruleTokens.FirstOrDefault();
+                    if (ruleToken.KeyName != token.Key.Name)
+                        continue;
+
+                    P[1 + i * n + j * n * r] = true;
+                }
+            }
+        }
 
         return null;
-    }
-
-    private IEnumerable<INode> reduct(IEnumerable<INode> elements)
-    {
-        yield break;
     }
 }
