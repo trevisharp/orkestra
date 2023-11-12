a7f9ae90fdbf883f8969916c6eb6f185e858aa62
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2023-11-12 07:55:03 -0300

    Add implementations in LR1

--- res/dotnet/SyntacticAnalysis/LR1SyntacticAnalyzerBuilder.cs
+++ res/dotnet/SyntacticAnalysis/LR1SyntacticAnalyzerBuilder.cs
@@ -48,37 +48,97 @@ public class LR1SyntacticAnalyzerBuilder : ISyntacticAnalyzerBuilder
 
                 return data;
             })
-            .Prepend(new int[] { 0, dict[StartRule], 0})
-            .ToArray();
+            .Prepend(new int[] { 0, dict[StartRule], 1})
+            .ToList();
         
         List<List<int>> states = new();
+        extend(new() { 0 });
 
-        List<int> state = new();
-        state.Add(0);
-        closure(state);
-
-        foreach (var rule in state.Select(x => prods[x]))
+        foreach (var rule in states[0].Select(x => prods[x]))
         {
             Verbose.Info($"{rule[0]} -> {string.Join(' ', rule.Skip(1))}");
         }
 
         void closure(List<int> state)
         {
+            List<int> searchedRules = new();
+            foreach (var index in state)
+            {
+                var production = prods[index];
+                var rule = production[0];
+                if (searchedRules.Contains(rule))
+                    continue;
+                
+                searchedRules.Add(rule);
+            }
+
             for (int i = 0; i < state.Count; i++)
             {
                 var production = prods[state[i]];
                 var dot = production[^1];
                 var crr = production[dot];
+                if (crr >= rulesEnd)
+                    continue;
 
-                for (int j = 0; j < prods.Length; j++)
+                if (searchedRules.Contains(crr))
+                    continue;
+                searchedRules.Add(crr);
+
+                for (int j = 0; j < prods.Count; j++)
                 {
                     var prod = prods[j];
+                    if (prod[^1] > 1)
+                        break;
+
                     if (prod[0] != crr)
                         continue;
                     
                     state.Add(j);
                 }
             }
+        }
+    
+        void extend(List<int> state)
+        {
+            closure(state);
+            states.Add(state);
+
+            List<int> expansionList = new();
+            foreach (var index in state)
+            {
+                var production = prods[index];
+                var dot = production[^1];
+                var expansion = production[dot];
+                if (expansionList.Contains(expansion))
+                    continue;
+                
+                expansionList.Add(expansion);
+            }
+
+            foreach (var expansion in expansionList)
+                expand(state, expansion);
+        }
+
+        void expand(List<int> state, int expansion)
+        {
+            List<int> newState = new();
+            foreach (var index in state)
+            {
+                var pro = prods[index];
+                var dot = pro[^1];
+                var exp = pro[dot];
+                if (exp != expansion)
+                    continue;
+                
+                var newRule = (int[])pro.Clone();
+                newRule[^1]++;
+                prods.Add(newRule);
+                // TODO: Treat item repetition
+                newState.Add(prods.Count - 1);
+            }
+            // TODO: Treat state repetition
+            extend(newState);
+            states.Add(newState);
         }
     }
 
