c46d46b5a7bf556f511e1f0aa5f99874555aefb2
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2024-02-22 07:54:46 -0300

    Move Token to SyntacticAnalysis namespace

---
+++ src/SyntacticAnalysis/Token.cs
@@ -0,0 +1,18 @@
+﻿/* Author:  Leonardo Trevisan Silio
+ * Date:    22/02/2024
+ */
+namespace Orkestra.SyntacticAnalysis;
+
+/// <summary>
+/// Represents a Token data output from lexical analisys.
+/// </summary>
+public record Token(Key Key, string Value, int Index) : INode
+{
+    public IRuleElement Element => Key;
+
+    public bool Is(IRuleElement token)
+        => token is Key key && key == this.Key;
+
+    public override string ToString()
+        => $"T:{Key.Name}";
+}
