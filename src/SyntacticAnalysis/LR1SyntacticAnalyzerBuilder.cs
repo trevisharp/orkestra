754a248c448e88654e302b57887d1c23a943a626
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2024-03-01 15:21:06 -0300

    Item set loading completed

--- src/SyntacticAnalysis/LR1SyntacticAnalyzerBuilder.cs
+++ src/SyntacticAnalysis/LR1SyntacticAnalyzerBuilder.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 
 namespace Orkestra.SyntacticAnalysis;
 
+using System.Runtime.CompilerServices;
 using InternalStructure;
 using Orkestra.InternalStructure;
 
@@ -34,38 +35,72 @@ public class LR1SyntacticAnalyzerBuilder : ISyntacticAnalyzerBuilder
         throw new System.NotImplementedException();
     }
 
-    int rulesLastIndex;
-    Dictionary<IRuleElement, int> elementMap;
-    Dictionary<int, IRuleElement> indexMap;
-    Dictionary<int, IEnumerable<SubRule>> subRuleMap;
-
-
     public void Load(IEnumerable<Key> keys)
     {
         int ruleCount = rules.Count();
         int keyCount = keys.Count();
         int indexSize = ruleCount + keyCount;
-        elementMap = new (indexSize);
-        indexMap = new (indexSize);
-        subRuleMap = new();
-
-        int index = 0;
-        foreach (var rule in rules)
-        {
-            var ruleIndex = ++index;
-            this.elementMap.Add(rule, ruleIndex);
-            this.indexMap.Add(ruleIndex, rule);
-            this.subRuleMap.Add(ruleIndex, rule.SubRules);
-        }
+        Dictionary<int, IRuleElement> indexMap = new (indexSize);
+        Dictionary<int, IEnumerable<SubRule>> subRuleMap = new();
         
-        this.rulesLastIndex = index;
+        int index = 0;
+        Dictionary<IRuleElement, int> elementMap = new (indexSize);
         foreach (var key in keys)
         {
             var keyIndex = ++index;
-            this.elementMap.Add(key, keyIndex);
-            this.indexMap.Add(keyIndex, key);
+            elementMap.Add(key, keyIndex);
+            indexMap.Add(keyIndex, key);
+        }
+        var keyLastIndex = index;
+
+        foreach (var rule in rules)
+        {
+            var ruleIndex = ++index;
+            elementMap.Add(rule, ruleIndex);
+            indexMap.Add(ruleIndex, rule);
+            subRuleMap.Add(ruleIndex, rule.SubRules);
         }
 
         using FastBuffer<int> pool = new();
+        Dictionary<int, int[]> itemMap = [];
+        Dictionary<int, List<int>> ruleItemMap = new();
+
+        int itemIndex = 1;
+        foreach (var rule in rules)
+        {
+            var ruleIndex = elementMap[rule];
+            List<int> itemRules = new();
+            foreach (var sb in rule.SubRules)
+            {
+                var tokens = sb.RuleTokens.ToArray();
+                var ruleSize = tokens.Length;
+                var item = pool.Rent(ruleSize + 2);
+                item[0] = ruleIndex;
+                item[1] = 0;
+                for (int i = 2; i < item.Length; i++)
+                    item[i] = elementMap[tokens[i - 2]];
+                itemMap.Add(itemIndex, item);
+                itemIndex++;
+            }
+            ruleItemMap.Add(ruleIndex, itemRules);
+        }
+
+        var goal = pool.Rent(3);
+        goal[0] = 0; 
+        goal[1] = 0;
+        goal[2] = elementMap[StartRule];
+        itemMap.Add(0, goal);
+        ruleItemMap.Add(0, [0]);
+
+        Dictionary<int, int> moveItemMap = new();
+
+        Stack<List<(int item, int ahead)>> stack = new();
+        stack.Append([(0, -1)]);
+        while(stack.Count > 0)
+        {
+            var state = stack.Pop();
+
+            
+        }
     }
 }
