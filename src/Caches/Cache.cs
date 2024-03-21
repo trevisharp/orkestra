188e3a7f004dbd53fd88be0d487e7ccf57d9e05f
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2024-03-20 22:04:07 -0300

    Start Cache implementation

--- src/Caches/Cache.cs
+++ src/Caches/Cache.cs
@@ -3,10 +3,64 @@
  */
 using System;
 using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace Orkestra.Cache;
 
-public abstract class Cache
+public class Cache(string file)
 {
-    
+    public Task<bool> LastWriteTest()
+    {
+        var cache = getFileCache(file);
+
+        
+    }
+
+    private async Task<LastWriteJson> getLastWriteCache(string cache)
+    {
+        const string lastWriteCache = "lastWrite.json";
+        var cacheFile = Path.Combine(cache, lastWriteCache);
+        
+        var obj = await openJson<LastWriteJson>(cacheFile);
+        return obj;
+    }
+
+    private async Task<T> openJson<T>(string file)
+    {
+        var json = await File.ReadAllTextAsync(file);
+        var obj = JsonSerializer.Deserialize<T>(json);
+        return obj;
+    }
+
+    private string getFileCache(string filePath)
+    {
+        var fileName = Path.GetFileNameWithoutExtension(filePath);
+        var cacheFolder = getCacheFolderPath();
+        var fileCache = Path.Combine(cacheFolder, fileName);
+        if (Path.Exists(fileCache))
+            return fileCache;
+        
+        Directory.CreateDirectory(fileCache);
+        return fileCache;
+    }
+
+    private string getDefaultCacheFolderPath()
+    {
+        var basePath = Environment.CurrentDirectory;
+        const string cacheFolder = ".cache";
+        return Path.Combine(basePath, cacheFolder);
+    }
+
+    private string getCacheFolderPath()
+    {
+        var cacheFolder = getDefaultCacheFolderPath();
+        if (Path.Exists(cacheFolder))
+            return cacheFolder;
+        
+        Directory.CreateDirectory(cacheFolder);
+        return cacheFolder;
+    }
+
+    private record LastWriteJson(DateTime lastSave);
 }
