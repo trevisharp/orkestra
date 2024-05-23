d3e00e141dfe7f01a3c8249878a006d6b96df136
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2024-05-23 00:30:18 -0300

    solve bug in example

--- tst/bruteforce/Program.cs
+++ tst/bruteforce/Program.cs
@@ -25,14 +25,14 @@ public class BruteForceProject : Project<BruteForceProject>
 
 public class BruteForceCompiler : Compiler
 {
-    Key SUM = key("SUM", "\\+");
-    Key SUB = key("SUB", "\\-");
-    Key MUL = key("MUL", "\\*");
-    Key DIV = key("DIV", "\\/");
-    Key POW = key("POW", "\\^");
-    Key COMMA = key("COMMA", "\\,");
-    Key OPENPAR = key("OPENPAR", "\\(");
-    Key CLOSEPAR = key("CLOSEPAR", "\\)");
+    // Key SUM = key("SUM", "\\+");
+    // Key SUB = key("SUB", "\\-");
+    // Key MUL = key("MUL", "\\*");
+    // Key DIV = key("DIV", "\\/");
+    // Key POW = key("POW", "\\^");
+    // Key COMMA = key("COMMA", "\\,");
+    // Key OPENPAR = key("OPENPAR", "\\(");
+    // Key CLOSEPAR = key("CLOSEPAR", "\\)");
 
     Key SUBSET = keyword("subset");
     Key OF = keyword("of");
@@ -40,36 +40,36 @@ public class BruteForceCompiler : Compiler
     Key INT = keyword("int");
     Key RAT = keyword("rat");
     Key REAL = keyword("real");
-    Key IS = keyword("is");
-    Key AND = keyword("and");
-    Key OR = keyword("or");
-    Key NOT = keyword("not");
+    // Key IS = keyword("is");
+    // Key AND = keyword("and");
+    // Key OR = keyword("or");
+    // Key NOT = keyword("not");
     Key DEFINE = keyword("define");
     Key AS = keyword("as");
-    Key CONTAINS = keyword("contains");
-    Key IF = keyword("if");
-    Key THEN = keyword("then");
-    Key FOR = keyword("for");
-    Key ALL = keyword("all");
-    Key SOME = keyword("some");
-    Key IN = keyword("in");
-    Key CHECK = keyword("check");
-    Key CONSIDERING = keyword("considering");
-    Key GIVEN = keyword("given");
+    // Key CONTAINS = keyword("contains");
+    // Key IF = keyword("if");
+    // Key THEN = keyword("then");
+    // Key FOR = keyword("for");
+    // Key ALL = keyword("all");
+    // Key SOME = keyword("some");
+    // Key IN = keyword("in");
+    // Key CHECK = keyword("check");
+    // Key CONSIDERING = keyword("considering");
+    // Key GIVEN = keyword("given");
     
-    Key NUMBER = key("NUMBER", "-?[0-9][0-9\\.]+");
+    // Key NUMBER = key("NUMBER", "-?[0-9][0-9\\.]+");
     Key ID = identity("IDENT", "[a-z]+");
 
-    Rule set, op, exp, list, value, boolean,
-        cond, definition, inclusion, condinclusion,
-        forall, exists, test, tests, checking, import,
-        given, item, itens, program;
+    Rule set, //op, exp, list, value, boolean, given
+        definition, //cond, inclusion, condinclusion,
+        //forall, exists, test, tests, checking, import,
+        item, itens, program;
     
     public BruteForceCompiler()
     {
-        op = rule("op",
-            sub(SUM), sub(MUL), sub(SUB), sub(DIV), sub(POW)
-        );
+        // op = rule("op",
+        //     sub(SUM), sub(MUL), sub(SUB), sub(DIV), sub(POW)
+        // );
 
         set = rule("set");
         set.AddSubRules(
@@ -78,7 +78,7 @@ public class BruteForceCompiler : Compiler
         );
 
         definition = rule("definition");
-        set.AddSubRules(
+        definition.AddSubRules(
             sub(DEFINE, ID, AS, set)
         );
 
@@ -89,7 +89,7 @@ public class BruteForceCompiler : Compiler
 
         itens = rule("itens");
         itens.AddSubRules(
-            sub(itens),
+            sub(item),
             sub(item, itens)
         );
 
