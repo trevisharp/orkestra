fd08033b069e797af80aa29bfc3ee76727cf2739
Trevisharp <leonardotrevisansilio@gmail.com>
2024-06-24 14:30:20 -0300

    Improve Key API

--- src/Key.cs
+++ src/Key.cs
@@ -1,46 +1,63 @@
 /* Author:  Leonardo Trevisan Silio
- * Date:    11/03/2024
+ * Date:    24/06/2024
  */
+using System.Linq;
+
 namespace Orkestra;
 
 /// <summary>
 /// Represents a Key for syntactical analisys.
 /// </summary>
-public record Key : ISyntacticElement
+public class Key(
+    string name, string expression, 
+    bool contextual, bool identity, 
+    bool keyword, bool auto
+    ) : ISyntacticElement
 {
-    private Key(string name, string expression, 
-        bool contextual, bool identity, bool keyword, bool auto)
-    { 
-        this.Name = name;
-        this.Expression = expression;
-        this.IsContextual = contextual;
-        this.IsIdentity = identity;
-        this.IsKeyword = keyword;
-        this.IsAuto = auto;
-    }
-    
-    public static Key CreateKey(string name, string expression)
-        => new Key(name, expression, false, false, false, false);
-    
-    public static Key CreateKeyword(string name, string expression)
-        => new Key(name, expression, false, false, true, false);
-    
-    public static Key CreateIdentity(string name, string expression)
-        => new Key(name, expression, false, true, false, false);
-      
-    public static Key CreateAutoKeyword(string name)
-        => new Key(name, null, false, false, true, true);
 
-    public static Key CreateContextual(string name, string expression)
-        => new Key(name, expression, true, false, true, false);
-
-    public string Name { get; set; }
-    public string Expression { get; private set; }
-    public bool IsContextual { get; private set; }
-    public bool IsIdentity { get; private set; }
-    public bool IsKeyword { get; private set; }
-    public bool IsAuto { get; private set; }
+    public string Name { get; set; } = name;
+    public string Expression { get; private set; } = expression;
+    public bool IsContextual { get; private set; } = contextual;
+    public bool IsIdentity { get; private set; } = identity;
+    public bool IsKeyword { get; private set; } = keyword;
+    public bool IsAuto { get; private set; } = auto;
 
     public override string ToString()
         => $"K:{Name}";
+    
+    public static Key CreateKey(string name, string expression)
+        => new(name, expression, false, false, false, false);
+    
+    public static Key CreateKeyword(string name, string expression)
+        => new(name, expression, false, false, true, false);
+    
+    public static Key CreateIdentity(string name, string expression)
+        => new(name, expression, false, true, false, false);
+      
+    public static Key CreateAutoKeyword(string name)
+        => new(name, null, false, false, true, true);
+
+    public static Key CreateContextual(string name, string expression)
+        => new(name, expression, true, false, true, false);
+    
+    public static Key CreateKey(string expression)
+        => new(null, expression, false, false, false, false);
+    
+    public static Key CreateKeyword(string expression)
+        => new(null, expression, false, false, true, false);
+    
+    public static Key CreateIdentity(string expression)
+        => new(null, expression, false, true, false, false);
+    
+    public static Key CreateAutoKeyword()
+        => new(null, null, false, false, true, true);
+
+    public static Key CreateContextual(string expression)
+        => new(null, expression, true, false, true, false);
+    
+    public static implicit operator Key(string expression)
+    {
+        bool isKeyword = expression.All(char.IsAsciiLetter);
+        return new(null, expression, false, false, isKeyword, false);
+    }
 }
