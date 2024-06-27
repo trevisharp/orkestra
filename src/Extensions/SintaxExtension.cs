e3a0a29b3750fe992380ada6229c3b631a73ba9e
Trevisharp <leonardotrevisansilio@gmail.com>
2024-06-27 16:08:10 -0300

    Implements mult options in snippets

--- src/Extensions/SintaxExtension.cs
+++ src/Extensions/SintaxExtension.cs
@@ -11,13 +11,15 @@ namespace Orkestra.Extensions;
 
 public static class SintaxExtension
 {
+    const string newBody = "\",\n\"\\t";
     public static string GetNormalForm(this Key key, ref int index)
     {
         if (key.IsKeyword)
             return key.Expression;
         
+        // close snippet body, jump line and start another snippet body
         if (key.Expression is [ '{' or '}' or '[' or ']' ])
-            return key.Expression + "\n";
+            return key.Expression + newBody;
         
         index++;
         return $"${{{index}:{key.Name}}}";
@@ -29,12 +31,33 @@ public static class SintaxExtension
         return rule.GetNormalForm(ref index);
     }
 
+    public static string GetNormalForm(this Rule rule, ref int index)
+    {
+        if (rule.SubRules.All(r => r.RuleTokens.Count() == 1))
+        {
+            var exps = rule.SubRules
+                .Select(x => x.RuleTokens.FirstOrDefault())
+                .Select(x => x as Key)
+                .Select(x => x.Expression);
+            index++;
+            return $"${{{index}|{string.Join(',', exps)}|}}";
+        }
+
+        return rule.SubRules
+            .FirstOrDefault()?
+            .GetNormalForm(ref index)
+            ?? string.Empty;
+    }
+
     public static string GetNormalForm(this SubRule rule, ref int index)
     {
         var sb = new StringBuilder();
 
         foreach (var token in rule.RuleTokens)
         {
+            if (sb.Length > 40)
+                sb.Append(newBody);
+
             if (token is Key key)
             {
                 sb.Append(key.GetNormalForm(ref index));
@@ -44,22 +67,15 @@ public static class SintaxExtension
 
             if (token is Rule rul)
             {
-                if (rul.SubRules.All(r => r.RuleTokens.Count() == 1))
-                {
-                    
-                }
-
-                var srul = rul.SubRules
-                    .FirstOrDefault();
-                if (srul is null)
-                    continue;
-
-                sb.Append(srul.GetNormalForm(ref index));
+                sb.Append(rul.GetNormalForm(ref index));
+                sb.Append(" ");
                 continue;
             }
         }
 
-        return sb.ToString().Trim();
+        return sb.ToString()
+            .Replace("  ", " ")
+            .Trim();
     }
 
     public static int GetComplexity(this SubRule rule)
