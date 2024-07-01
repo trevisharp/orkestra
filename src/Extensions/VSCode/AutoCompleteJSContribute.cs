c9ceed4769f09d5765a66f4a4208760b734d85c1
Trevisharp <leonardotrevisansilio@gmail.com>
2024-07-01 09:58:16 -0300

    Improve AutoCompleteJSContribute organization. Add groups generations. Separe conent in many functions

--- src/Extensions/VSCode/AutoCompleteJSContribute.cs
+++ src/Extensions/VSCode/AutoCompleteJSContribute.cs
@@ -1,9 +1,11 @@
 /* Author:  Leonardo Trevisan Silio
  * Date:    28/06/2023
  */
+using System;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
-using Microsoft.VisualBasic;
+using Orkestra.SyntacticAnalysis;
 
 namespace Orkestra.Extensions.VSCode;
 
@@ -66,20 +68,80 @@ public class AutoCompleteJSContribute(LanguageInfo language) : JSContribute
 
     void process(IGrouping<string, SubRule> group, StringBuilder sb, int index)
     {
-        sb.AppendLine(
+        Verbose.Success(group.Key);
+        Verbose.NewLine();
+        Verbose.StartGroup();
+        foreach (var item in group)
+        {
+            foreach (var el in item)
+                Verbose.InlineContent(el.Name);
+            Verbose.NewLine();
+        }
+        Verbose.EndGroup();
+        Verbose.NewLine();
+
+        var code = getCode(group, index);
+        if (code is null)
+            return;
+        
+        sb.AppendLine(code);
+    }
+
+    string getCode(IGrouping<string, SubRule> group, int index)
+    {
+        if (group.All(g => g.Count() == 1))
+            return getSimpleAutoComplete(group, index);
+        
+        if (group.Count() == 1)
+            return getComplexUnique(group, index);
+
+        return null;
+    }
+
+    string getSimpleAutoComplete(IGrouping<string, SubRule> group, int index)
+    {
+        var subRule = group.FirstOrDefault();
+        if (subRule is null)
+            return string.Empty;
+        
+        var header = subRule.FirstOrDefault() as Key;
+        if (header is null)
+            return string.Empty;
+
+        return registerCompletionItemProvider(index,
+            $$"""
+            const comp = new vscode.CompletionItem('{{header.Expression}}');
+
+            return [ comp ];
+            """
+        );
+    }
+
+    string getComplexUnique(IGrouping<string, SubRule> group, int index)
+    {
+        var rule = group.FirstOrDefault();
+        if (rule is null)
+            return string.Empty;
+
+        return
+            $$"""
+                
+            """;
+    }
+
+    string registerCompletionItemProvider(int index, string code)
+    {
+        return
             $$"""
             const provider{{index}} = vscode.languages.registerCompletionItemProvider('{{language.Name}}', {
 
                 provideCompletionItems(document, position) {
+                
+            {{code.Replace("\n", "\n\t\t")}}
 
-                    a
-
-                    retrun [];
                 }
-
             });
             context.subscriptions.push(provider{{index}});
-            """
-        );
+            """;
     }
 }
