ad7b695b0c6dd29b5b59ac8ba69c46a27e1f5056
trevisharp <leonardotrevisansilio@gmail.com>
2025-02-07 14:46:06 -0300

    flexibilize the CHANGELOG.md generation

--- Orkestra/Extensions/ExtensionArguments.cs
+++ Orkestra/Extensions/ExtensionArguments.cs
@@ -5,11 +5,14 @@ using System.Collections.Generic;
 
 namespace Orkestra.Extensions;
 
+using VSCode;
+
 /// <summary>
 /// Represents parameters to create a item.
 /// </summary>
 public class ExtensionArguments {
     public required string Name { get; set; }
     public required string[] Arguments { get; set; }
-    public List<LanguageInfo> Languages { get; set; } = new();
+    public List<LanguageInfo> Languages { get; set; } = [];
+    public Changelog Changelog { get; set; } = Changelog.Default;
 }
